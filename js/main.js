ed6ddd1374f1e996176523bed4e387a316c1611c
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-09 17:22:36 +0200

    add main.js -_-

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function add(a,b) {return(a+b)};
+function add(a,b) {return(parseFloat(a)+parseFloat(b))};
 function subtract(a,b) {return(a-b)};
 function multiply(a,b) {return(a*b)};
 function divide(a,b) {return(a/b)};
@@ -8,3 +8,38 @@ function operate(operator, a, b){
         a, b
       );
 }
+const display = document.querySelector("#numbers");
+let displayValue = "";
+let ans = 0;
+let nextOperation = "";
+const digits = document.querySelectorAll(".digits>*");
+
+digits.forEach(digit => {
+  digit.addEventListener("click",e => {
+    displayValue == "0" ? displayValue ="" : displayValue=displayValue;
+    displayValue += e.target.textContent;
+    display.textContent = displayValue;
+  });
+});
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click",() => {
+  displayValue = "0";
+  display.textContent = displayValue;
+});
+const operators = document.querySelectorAll(".operators > .math");
+operators.forEach(operator => {
+  operator.addEventListener("click",e => {
+    nextOperation = e.target.id;
+    ans = displayValue;
+    displayValue = "";
+    display.textContent = displayValue;
+  })
+});
+const equal = document.querySelector("#equal");
+equal.addEventListener("click", e =>{
+  ans = operate(nextOperation,ans,displayValue);
+  //fixed number after coma
+  ans = +ans.toFixed(5);
+  displayValue = ans;
+  display.textContent = displayValue;
+})
